6a903532c479af142136a00f2136b97406ed5433
Mutlu Akgun <78053007+MutluRenegado@users.noreply.github.com>
2025-03-22 18:33:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+export default {
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+    sessionSecret: process.env.SESSION_SECRET,
+    port: process.env.PORT || 3000
+};
